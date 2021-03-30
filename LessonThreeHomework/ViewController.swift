55bfcae99eafa97e3a3c81be6dd0aa1f589fb9a0
Philip4000 <6427676@bk.ru>
2021-03-30 18:03:56 +0300

    Initial Commit

---
+++ LessonThreeHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonThreeHomework
+//
+//  Created by Philip Noskov on 30.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
